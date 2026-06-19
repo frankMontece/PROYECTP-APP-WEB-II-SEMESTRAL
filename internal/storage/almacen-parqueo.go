96a0f4c16dbd14a79af2e0243a25ddb5066ba4e4
WinterPovea <bravopwinter@gmail.com>
2026-06-19 14:40:10 -0500

    feat: definir interfaces de repositorio para vehículos, visitas y accesos en AlmacenParqueo

--- internal/storage/almacen-parqueo.go
+++ internal/storage/almacen-parqueo.go
@@ -2,27 +2,37 @@ package storage
 
 import "condominio-api/internal/models"
 
-type AlmacenParqueo interface {
-	//Vehiculos de residentes
+//Interfaz de repositorio vehículos
+type VehiculosRepository interface {
 	ListarVehiculos() []models.Vehiculo
 	BuscarVehiculoPorID(id uint) (models.Vehiculo, bool)
 	CrearVehiculo(v models.Vehiculo) models.Vehiculo
 	ActualizarVehiculo(id uint, datos models.Vehiculo) (models.Vehiculo, bool)
 	BorrarVehiculo(id uint) bool
+}
 
-	//Visitas de vehículos
+//Interfaz de repositorio visitas
+type VisitasRepository interface {
 	ListarVisitas() []models.VisitaVehiculo
 	BuscarVisitaPorID(id uint) (models.VisitaVehiculo, bool)
 	CrearVisita(v models.VisitaVehiculo) models.VisitaVehiculo
 	ActualizarVisita(id uint, datos models.VisitaVehiculo) (models.VisitaVehiculo, bool)
 	RegistrarSalidaVisita(id uint) (models.VisitaVehiculo, bool)
 	BorrarVisita(id uint) bool
+}
 
-	//Bitácora de accesos
+//Interfaz de repositorio accesos
+type AccesosRepository interface {
 	ListarAccesos() []models.AccesoVehiculo
 	BuscarAccesoPorID(id uint) (models.AccesoVehiculo, bool)
 	CrearAcceso(a models.AccesoVehiculo) models.AccesoVehiculo
 	BorrarAcceso(id uint) bool
+}
+
+type AlmacenParqueo interface {
+	VehiculosRepository
+	VisitasRepository
+	AccesosRepository
 }
 
 var _ AlmacenParqueo = (*MemoriaParqueo)(nil)
