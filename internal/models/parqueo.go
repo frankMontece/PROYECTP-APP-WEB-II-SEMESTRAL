37d466c48c870061b36ec017f3d91563441b1589
WinterPovea <bravopwinter@gmail.com>
2026-07-07 15:09:02 -0500

    feat: enhance GORM tags for vehicle models in parqueo.go

--- internal/models/parqueo.go
+++ internal/models/parqueo.go
@@ -3,34 +3,34 @@ package models
 import "time"
 
 type Vehiculo struct {
-	ID          uint      `json:"id"`
-	ResidenteID uint      `json:"residente_id"`
-	Placa       string    `json:"placa"`
-	Marca       string    `json:"marca"`
+	ID          uint      `json:"id" gorm:"primaryKey"`
+	ResidenteID uint      `json:"residente_id" gorm:"not null;index"`
+	Placa       string    `json:"placa" gorm:"unique;not null"`
+	Marca       string    `json:"marca" gorm:"not null"`
 	Modelo      string    `json:"modelo"`
 	Color       string    `json:"color"`
-	Activo      bool      `json:"activo"`
+	Activo      bool      `json:"activo;default:true"`
 	CreatedAt   time.Time `json:"created_at"`
 }
 
 type VisitaVehiculo struct {
-	ID              uint       `json:"id"`
-	CondominioID    uint       `json:"condominio_id"`
-	ResidenteID     uint       `json:"residente_id"`
-	PlacaVisitante  string     `json:"placa_visitante"`
-	NombreVisitante string     `json:"nombre_visitante"`
+	ID              uint       `json:"id" gorm:"primaryKey"`
+	CondominioID    uint       `json:"condominio_id" gorm:"not null;index"`
+	ResidenteID     uint       `json:"residente_id" gorm:"not null;index"`
+	PlacaVisitante  string     `json:"placa_visitante" gorm:"not null"`
+	NombreVisitante string     `json:"nombre_visitante" gorm:"not null"`
 	HoraEntrada     *time.Time `json:"hora_entrada"`
 	HoraSalida      *time.Time `json:"hora_salida"`
 	Motivo          string     `json:"motivo"`
-	CodigoQR        string     `json:"codigo_qr"`
+	CodigoQR        string     `json:"codigo_qr" gorm:"unique"`
 	EstadoQR        string     `json:"estado_qr"`
 }
 
 type AccesoVehiculo struct {
-	ID             uint      `json:"id"`
-	VehiculoID     uint      `json:"vehiculo_id"`
-	CondominioID   uint      `json:"condominio_id"`
-	TipoMovimiento string    `json:"tipo_movimiento"`
+	ID             uint      `json:"id" gorm:"primaryKey"`
+	VehiculoID     uint      `json:"vehiculo_id" gorm:"not null;index"`
+	CondominioID   uint      `json:"condominio_id" gorm:"not null;index"`
+	TipoMovimiento string    `json:"tipo_movimiento" gorm:"not null"`
 	FechaHora      time.Time `json:"fecha_hora"`
 	Observacion    string    `json:"observacion"`
 }
