487b4614643c537d86020850538c68ba66a8f4e3
WinterPovea <bravopwinter@gmail.com>
2026-07-08 15:30:50 -0500

    feat: add storage interfaces for vehicles, visits, and access management

---
+++ internal/storage/almacen-parqueo.go
@@ -0,0 +1,42 @@
+package storage
+
+import "condominio-api/internal/models"
+
+// Interfaz de repositorio vehículos
+type VehiculosRepository interface {
+	ListarVehiculos() []models.Vehiculo
+	BuscarVehiculoPorID(id uint) (models.Vehiculo, bool)
+	CrearVehiculo(v models.Vehiculo) models.Vehiculo
+	ActualizarVehiculo(id uint, datos models.Vehiculo) (models.Vehiculo, bool)
+	BorrarVehiculo(id uint) bool
+}
+
+// Interfaz de repositorio visitas
+type VisitasRepository interface {
+	ListarVisitas() []models.VisitaVehiculo
+	BuscarVisitaPorID(id uint) (models.VisitaVehiculo, bool)
+	CrearVisita(v models.VisitaVehiculo) models.VisitaVehiculo
+	ActualizarVisita(id uint, datos models.VisitaVehiculo) (models.VisitaVehiculo, bool)
+	RegistrarSalidaVisita(id uint) (models.VisitaVehiculo, bool)
+	BorrarVisita(id uint) bool
+}
+
+// Interfaz de repositorio accesos
+type AccesosRepository interface {
+	ListarAccesos() []models.AccesoVehiculo
+	BuscarAccesoPorID(id uint) (models.AccesoVehiculo, bool)
+	CrearAcceso(a models.AccesoVehiculo) models.AccesoVehiculo
+	BorrarAcceso(id uint) bool
+}
+
+type AlmacenParqueo interface {
+	VehiculosRepository
+	VisitasRepository
+	AccesosRepository
+}
+
+var _ VehiculosRepository = (*VehiculoSQLite)(nil)
+
+var _ VisitasRepository = (*VisitaSQLite)(nil)
+
+var _ AccesosRepository = (*AccesoSQLite)(nil)
